1533547a70e03a90ee57900a9aa59a5c11597b80
carneirofc <claudiofcarneiro@hotmail.com>
2021-01-18 16:19:15 -0300

    Removed unused users/ module @may return in the future but completly different

--- src/users/index.js
+++
@@ -1,9 +0,0 @@
-import makeDB from "../db";
-import makeController from "./controller";
-import makeHandler from "./handler";
-
-const database = makeDB();
-const controller = makeController(database);
-const handler = makeHandler(controller);
-
-export default handler;
