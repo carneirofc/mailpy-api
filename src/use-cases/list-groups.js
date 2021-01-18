11d5e18c5f13f6f4536dce80977d760cab1555e7
carneirofc <claudiofcarneiro@hotmail.com>
2021-01-18 16:17:28 -0300

    Include basic use-cases/ modules

---
+++ src/use-cases/list-groups.js
@@ -0,0 +1,7 @@
+const makeListGroups = ({ mailpyDb }) => {
+  return async function () {
+    const groups = await mailpyDb.findAllGroups();
+    return groups;
+  }
+};
+export default makeListGroups;
