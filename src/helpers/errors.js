5e974cd7e6b7a13f4b716a90a7c5598bb4b6ba2c
carneirofc <claudiofcarneiro@hotmail.com>
2021-01-22 09:55:32 -0300

    Include data-acceess errors

--- src/helpers/errors.js
+++ src/helpers/errors.js
@@ -1,9 +1,26 @@
 export class InvalidPropertyError extends Error {
     constructor(msg) {
         super(msg);
-
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, InvalidPropertyError);
+        }
+    }
+}
+
+export class DatabaseUnexpectedError extends Error {
+    constructor(msg) {
+        super(`data-access failure, unexpected error: ${msg}`);
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, DatabaseUnexpectedError);
+        }
+    }
+}
+
+export class DatabaseDuplicatedKeyError extends Error {
+    constructor(msg) {
+        super(`data-access failure: ${msg}`);
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, DatabaseDuplicatedKeyError);
         }
     }
 }
