3969b058fd6048852bb2d39542717d11a48f96e1
carneirofc <claudiofcarneiro@hotmail.com>
2021-01-22 22:30:46 -0300

    User login, in progress

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -4,13 +4,21 @@ import makeListConditions from "./list-conditions";
 import makeListGroups from "./list-groups";
 import makeListEntries from "./list-entries";
 import makeUserLogin from "./login-user";
+import makeAddUser from "./add-user";
 
 import { getAzureUserInfo as getExternalUserInfo } from "../helpers/azure";
 
 export const listConditions = makeListConditions({ mailpyDb });
 export const listGroups = makeListGroups({ mailpyDb });
 export const listEntries = makeListEntries({ mailpyDb });
-export const userLogin = makeUserLogin({ mailpyDb, getExternalUserInfo });
+export const addUser = makeAddUser({ mailpyDb });
+
+export const userLogin = makeUserLogin({
+  mailpyDb,
+  getExternalUserInfo,
+  addUser,
+  makeUser
+});
 
 const mailpyService = Object.freeze({
   listConditions,
