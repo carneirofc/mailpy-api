f2a63798e8fb71fa6716d90958732d472da3339c
carneirofc <claudiofcarneiro@hotmail.com>
2021-01-22 22:30:11 -0300

    Update tests, convert _id ObjectID to id as string

--- src/data-access/mailpy-db.test.js
+++ src/data-access/mailpy-db.test.js
@@ -33,27 +33,27 @@ describe("mailpy db", () => {
     });
   })
 
-  it("List conditions", async () => {
+  it("find all conditions", async () => {
     const conditions = await mailpyDb.findAllConditions();
     expect(conditions.length).toBe(Object.keys(conditionsEnum).length);
   })
 
-  it("List grants", async () => {
+  it("find all grants", async () => {
     const grants = await mailpyDb.findAllGrants();
     expect(grants.length).toBe(Object.keys(grantsEnum).length);
   })
 
-  it("List initial roles", async () => {
+  it("find all roles", async () => {
     const roles = await mailpyDb.findAllRoles();
     expect(roles.length).toBe(Object.keys(defaultRoles).length);
   })
 
-  it("Add a user", async () => {
+  it("insert user", async () => {
     const user = makeUser({
       name: faker.name.findName(),
       uuid: faker.random.uuid()
     });
-    const result = await mailpyDb.insertUser({
+    let result = await mailpyDb.insertUser({
       uuid: user.uuid,
       name: user.name,
       email: user.email,
@@ -73,8 +73,11 @@ describe("mailpy db", () => {
       });
     }).rejects.toThrow(DatabaseDuplicatedKeyError)
 
-    const result = await mailpyDb.deleteUserByUUID({ uuid: user.uuid });
+    result = await mailpyDb.deleteUserByUUID({ uuid: user.uuid });
     expect(result).toBe(1);
+
+    result = await mailpyDb.deleteUserByUUID({ uuid: user.uuid });
+    expect(result).toBe(0);
   })
 
 })
