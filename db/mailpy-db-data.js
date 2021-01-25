2e5f98f0ba99c2a7d623c10bd47fe8a6edb7836c
carneirofc <claudiofcarneiro@hotmail.com>
2021-01-25 10:31:04 -0300

    Update database setup scripts

--- db/mailpy-db-data.js
+++ db/mailpy-db-data.js
@@ -1,53 +1,99 @@
-import {
-  collections
-} from "./mailpy-db-setup";
+import { ObjectID } from "mongodb";
+import { collections } from "./mailpy-db-setup";
 
-const {
-  conditions,
-  entries,
-  grants,
-  groups,
-  roles,
-  users
-} = collections;
+const { conditions, entries, grants, groups, roles, users } = collections;
 
 export const conditionsEnum = {
-  "out of range": { desc: "Must remain within the specified range." },
-  "superior than": { desc: "Must remain superior than." },
-  "inferior than": { desc: "Must remain inferior than." },
-  "increasing step": { desc: "Each increasing step triggers an alarm." },
-  "decreasing step": { desc: "Each decreasing step triggers an alarm." },
+  outOfrange: {
+    _id: new ObjectID(),
+    name: "out of range",
+    desc: "Must remain within the specified range.",
+  },
+  superiorThan: {
+    _id: new ObjectID(),
+    name: "superior than",
+    desc: "Must remain superior than.",
+  },
+  inferiorThan: {
+    _id: new ObjectID(),
+    name: "inferior than",
+    desc: "Must remain inferior than.",
+  },
+  increasingStep: {
+    _id: new ObjectID(),
+    name: "increasing step",
+    desc: "Each increasing step triggers an alarm.",
+  },
+  decreasingStep: {
+    _id: new ObjectID(),
+    name: "decreasing step",
+    desc: "Each decreasing step triggers an alarm.",
+  },
 };
 
 export const grantsEnum = {
-  "entries.delete": { desc: "Delete an entry" },
-  "entries.insert": { desc: "Insert a new entry" },
-  "entries.update": { desc: "Update every aspect of an entry" },
-  "groups.delete": { desc: "Delete a group" },
-  "groups.disable": { desc: "Disable monitoring for a group" },
-  "groups.insert": { desc: "Add groups" },
-  "groups.update": { desc: "Update every aspect of a group" },
-  "users.update": { desc: "Update user attributes" },
+  entriesDelete: {
+    _id: new ObjectID(),
+    name: "entries.delete",
+    desc: "Delete an entry",
+  },
+  entriesInsert: {
+    _id: new ObjectID(),
+    name: "entries.insert",
+    desc: "Insert a new entry",
+  },
+  entriesUpdate: {
+    _id: new ObjectID(),
+    name: "entries.update",
+    desc: "Update every aspect of an entry",
+  },
+  groupsDelete: {
+    _id: new ObjectID(),
+    name: "groups.delete",
+    desc: "Delete a group",
+  },
+  groupsDisable: {
+    _id: new ObjectID(),
+    name: "groups.disable",
+    desc: "Disable monitoring for a group",
+  },
+  groupsInsert: {
+    _id: new ObjectID(),
+    name: "groups.insert",
+    desc: "Add groups",
+  },
+  groupsUpdate: {
+    _id: new ObjectID(),
+    name: "groups.update",
+    desc: "Update every aspect of a group",
+  },
+  usersUpdate: {
+    _id: new ObjectID(),
+    name: "users.update",
+    desc: "Update user attributes",
+  },
 };
 
 export const defaultRoles = {
   Admin: {
+    name: "Admin",
     desc: "Full control over the system",
     grants: [
-      grantsEnum["entries.delete"],
-      grantsEnum["entries.insert"],
-      grantsEnum["entries.update"],
-      grantsEnum["groups.delete"],
-      grantsEnum["groups.disable"],
-      grantsEnum["groups.insert"],
-      grantsEnum["groups.update"],
-      grantsEnum["users.update"]
-    ]
+      grantsEnum.entriesDelete._id,
+      grantsEnum.entriesInsert._id,
+      grantsEnum.entriesUpdate._id,
+      grantsEnum.groupsDelete._id,
+      grantsEnum.groupsDisable._id,
+      grantsEnum.groupsInsert._id,
+      grantsEnum.groupsUpdate._id,
+      grantsEnum.usersUpdate._id,
+    ],
   },
   Guest: {
+    name: "Guest",
     desc: "Read-only",
-    grants: []
-  }
+    grants: [],
+  },
 };
 
 const listFromObjects = (objs) => {
@@ -55,29 +101,28 @@ const listFromObjects = (objs) => {
   const items = [];
   for (let name in objs) {
     const obj = objs[name];
-    items.push({ ...obj, name: name });
+    items.push({ ...obj });
   }
   return items;
-}
+};
 
 const insertConditions = async (db) => {
   const collection = db.collection(conditions);
   const items = listFromObjects(conditionsEnum);
   return collection.insertMany(items);
-}
+};
 
 const insertGrants = async (db) => {
   const collection = db.collection(grants);
   const items = listFromObjects(grantsEnum);
   return await collection.insertMany(items);
-}
+};
 
 const insertRoles = async (db) => {
   const collection = db.collection(roles);
   const items = listFromObjects(defaultRoles);
   return await collection.insertMany(items);
-}
-
+};
 
 export default function makeMailpyDbData({ makeDb }) {
   const insertData = async () => {
@@ -87,10 +132,9 @@ export default function makeMailpyDbData({ makeDb }) {
     await insertConditions(db);
     await insertGrants(db);
     await insertRoles(db);
-
-  }
+  };
 
   return Object.freeze({
-    insertData
+    insertData,
   });
 }
