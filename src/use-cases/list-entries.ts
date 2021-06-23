b7588866c6678c398438d858f51b46b6f405256c
carneirofc <claudiofcarneiro@hotmail.com>
2021-06-22 23:00:32 -0300

    use-cases and deps

--- src/use-cases/list-entries.ts
+++ src/use-cases/list-entries.ts
@@ -1,4 +1,6 @@
-const makeListEntries = ({ mailpyDb }) => {
+import { MailpyDB } from "../data-access/mailpy-db";
+
+const makeListEntries = ({ mailpyDb }: { mailpyDb: MailpyDB }) => {
   return async function () {
     const entries = await mailpyDb.findAllEntries();
     return entries;
--- src/use-cases/list-groups.ts
+++ src/use-cases/list-groups.ts
@@ -1,4 +1,6 @@
-const makeListGroups = ({ mailpyDb }) => {
+import { MailpyDB } from "../data-access/mailpy-db";
+
+const makeListGroups = ({ mailpyDb }: { mailpyDb: MailpyDB }) => {
   return async function () {
     const groups = await mailpyDb.findAllGroups();
     return groups;
