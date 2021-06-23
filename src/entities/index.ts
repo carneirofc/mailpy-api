105cd5d0e2d17df601a0da87369cf19d27522d7e
carneirofc <claudiofcarneiro@hotmail.com>
2021-06-23 14:15:27 -0300

    prettier

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -2,7 +2,7 @@ import buildMakeUser from "./user";
 import buildMakeGroup from "./mailpy";
 
 export * from "./user";
-export * from "./mailpy"
+export * from "./mailpy";
 
 export const makeUser = buildMakeUser({});
 export const makeGroup = buildMakeGroup({});
