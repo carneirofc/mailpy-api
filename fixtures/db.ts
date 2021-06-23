9a453d01ba60de607ae69a288414d69cebff6ca4
carneirofc <claudiofcarneiro@hotmail.com>
2021-06-23 10:22:16 -0300

    Update db fixture

--- fixtures/db.ts
+++ fixtures/db.ts
@@ -3,16 +3,23 @@ import { Db, MongoClient } from "mongodb";
 let connection: MongoClient = null;
 let db: Db = null;
 
+const MONGO_DB_URI: string = (global as any).__MONGO_URI__;
+const MONGO_DB_NAME: string = (global as any).__MONGO_DB_NAME;
+
 export default async function makeDb(): Promise<Db> {
-  connection =
-    connection ||
-    (await MongoClient.connect((global as any).__MONGO_URI__, { useNewUrlParser: true, useUnifiedTopology: true }));
-  db = db || connection.db((global as any).__MONGO_DB_NAME);
+  if (connection === null) {
+    connection = await MongoClient.connect(MONGO_DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+  if (db === null) {
+    db = connection.db(MONGO_DB_NAME);
+  }
   return db;
 }
 
 export const closeDb = async () => {
   await connection.close();
-  // await db.close(); // No need to ?
 };
 export { connection, db };
