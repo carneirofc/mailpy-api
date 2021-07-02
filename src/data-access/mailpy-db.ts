a3f3080d50dca5f5ab608bfa9f2778cc062cee0a
carneirofc <claudiofcarneiro@hotmail.com>
2021-07-02 09:22:11 -0300

    data-access: entry update and delete

--- src/data-access/mailpy-db.ts
+++ src/data-access/mailpy-db.ts
@@ -1,8 +1,18 @@
-import { Condition, ConditionName, Entry, Group, makeCondition, makeEntry, makeGroup } from "../entities";
+import {
+  Condition,
+  ConditionName,
+  ConditionNameHas,
+  Entry,
+  Group,
+  makeCondition,
+  makeEntry,
+  makeGroup,
+} from "../entities";
 import { collections } from "../../fixtures/db/mailpy-db-setup";
 import { MakeDb } from "./interfaces";
 import { ObjectId } from "mongodb";
 import { DatabaseDuplicatedKeyError, DatabaseError, InvalidPropertyError } from "../helpers/errors";
+import { deepCopy } from "../helpers/deep-copy";
 import { CodeDuplicatedKey } from "./error-codes";
 
 const { conditions, entries, groups } = collections;
@@ -13,6 +23,7 @@ export interface MailpyDB {
   findAllGroups: () => Promise<Group[]>;
 
   findConditionById: (id: string) => Promise<Condition>;
+  findConditionByName: (name: string) => Promise<Condition>;
   findEntryById: (id: string) => Promise<Entry>;
   findGroupById: (id: string) => Promise<Group>;
 
@@ -30,15 +41,26 @@ export interface MailpyDB {
 }
 
 export default function makeMailpyDb({ makeDb }: { makeDb: MakeDb }): MailpyDB {
-  type GroupJsonObj = { _id: ObjectId; name: string; desc: string; enabled: boolean };
-  type ConditionJsonObj = { _id: ObjectId; name: ConditionName; desc: string };
+  type GroupJsonObj = {
+    _id: ObjectId;
+    name: string;
+    desc: string;
+    enabled: boolean;
+  };
+  type ConditionJsonObj = {
+    _id: ObjectId;
+    name: ConditionName;
+    desc: string;
+  };
   type EntryJsonObj = {
     _id: ObjectId;
     alarm_values: string;
-    condition: ConditionJsonObj;
+    condition: string;
+    condition_lookup?: ConditionJsonObj;
     email_timeout: number;
     emails: string;
-    group: GroupJsonObj;
+    group: string;
+    group_lookup?: GroupJsonObj;
     pvname: string;
     subject: string;
     unit: string;
@@ -48,15 +70,20 @@ export default function makeMailpyDb({ makeDb }: { makeDb: MakeDb }): MailpyDB {
   function parseGroup({ _id, name, desc, enabled }: GroupJsonObj): Group {
     return makeGroup({ id: _id.toHexString(), desc, name, enabled });
   }
+
   function parseCondition({ _id, name, desc }: ConditionJsonObj): Condition {
+    if (!ConditionNameHas(name)) {
+      throw new InvalidPropertyError(`Condition name "${name}" is invalid ${ConditionName}`);
+    }
     return { id: _id.toHexString(), name, desc };
   }
-  function parseEntry({ _id, condition, emails, group, ...data }: EntryJsonObj): Entry {
+
+  function parseEntry({ _id, condition, group, condition_lookup, emails, group_lookup, ...data }: EntryJsonObj): Entry {
     return makeEntry({
       id: _id.toHexString(),
       emails: emails.split(";"),
-      group: parseGroup(group),
-      condition: parseCondition(condition),
+      group: parseGroup(group_lookup),
+      condition: parseCondition(condition_lookup),
       ...data,
     });
   }
@@ -98,11 +125,52 @@ export default function makeMailpyDb({ makeDb }: { makeDb: MakeDb }): MailpyDB {
     }
 
     async updateEntry(entry: Entry): Promise<Entry> {
-      throw "Not Impplemented";
+      if (entry.id === undefined || entry.id === null) {
+        throw new InvalidPropertyError(`Failed to update entry, id cannot be empty`);
+      }
+
+      const db = await makeDb();
+      const { id, group, condition, emails, ...data } = entry;
+      const res = await db.collection<EntryJsonObj>(entries).updateOne(
+        { _id: new ObjectId(entry.id) },
+        {
+          $set: {
+            ...data,
+            emails: emails.join(";"),
+            condition: condition.name,
+            group: group.name,
+          },
+        }
+      );
+      if (res.result.nModified !== 1) {
+        throw new DatabaseError(`Failure on group update "${group}"`);
+      }
+      return await this.findEntryById(entry.id);
     }
 
     async createEntry(entry: Entry): Promise<Entry> {
-      throw "Not Impplemented";
+      const db = await makeDb();
+
+      const groupCheck = await db.collection<GroupJsonObj>(groups).findOne({ _id: new ObjectId(entry.group.id) });
+      if (!groupCheck) {
+        throw new InvalidPropertyError(`Cannot create entry, group is invalid "${entry.group.id} ${entry.group.name}"`);
+      }
+
+      const res = await db.collection<EntryJsonObj>(entries).insertOne({
+        alarm_values: entry.alarm_values,
+        email_timeout: entry.email_timeout,
+        emails: entry.emails.join(";"),
+        pvname: entry.pvname,
+        warning_message: entry.warning_message,
+        subject: entry.subject,
+        unit: entry.unit,
+        condition: entry.condition.name,
+        group: entry.group.name,
+      });
+      const entryResult = res.ops[0];
+      const newEntry = deepCopy(entry);
+      newEntry.id = entryResult._id.toHexString();
+      return newEntry;
     }
 
     async createGroup(group: Group): Promise<Group> {
@@ -125,14 +193,34 @@ export default function makeMailpyDb({ makeDb }: { makeDb: MakeDb }): MailpyDB {
       return parseGroup(res.ops[0]);
     }
 
+    async findConditionByName(name: ConditionName): Promise<Condition> {
+      const db = await makeDb();
+      const res = await db.collection<ConditionJsonObj>(conditions).findOne({ name: name });
+      return parseCondition(res);
+    }
+
     async findConditionById(id: string): Promise<Condition> {
       const db = await makeDb();
-      throw "Not Impplemented";
+      const res = await db.collection<ConditionJsonObj>(conditions).findOne({ _id: new ObjectId(id) });
+      return parseCondition(res);
     }
 
     async findEntryById(id: string): Promise<Entry> {
       const db = await makeDb();
-      throw "Not Impplemented";
+      const res = await db
+        .collection<EntryJsonObj>(entries)
+        .aggregate([
+          { $match: { _id: new ObjectId(id) } },
+          { $lookup: { from: conditions, localField: "condition", foreignField: "name", as: "condition_lookup" } },
+          { $unwind: { path: "$condition_lookup", preserveNullAndEmptyArrays: true } },
+          { $lookup: { from: groups, localField: "group", foreignField: "name", as: "group_lookup" } },
+          { $unwind: { path: "$group_lookup", preserveNullAndEmptyArrays: true } },
+        ])
+        .toArray();
+      if (res.length !== 1) {
+        return null;
+      }
+      return parseEntry(res[0]);
     }
 
     async findGroupById(id: string): Promise<Group> {
@@ -163,7 +251,7 @@ export default function makeMailpyDb({ makeDb }: { makeDb: MakeDb }): MailpyDB {
               from: "groups",
               localField: "group",
               foreignField: "name",
-              as: "group",
+              as: "group_lookup",
             },
           },
           {
@@ -171,7 +259,7 @@ export default function makeMailpyDb({ makeDb }: { makeDb: MakeDb }): MailpyDB {
               from: "conditions",
               localField: "condition",
               foreignField: "name",
-              as: "condition",
+              as: "condition_lookup",
             },
           },
         ])
