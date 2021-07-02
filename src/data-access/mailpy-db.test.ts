0115bed027c60cbb93ab28ec708fe2fd300c4287
carneirofc <claudiofcarneiro@hotmail.com>
2021-07-02 09:22:11 -0300

    data-access: entry update and delete

--- src/data-access/mailpy-db.test.ts
+++ src/data-access/mailpy-db.test.ts
@@ -1,11 +1,11 @@
-import faker from "faker";
+import faker, { fake } from "faker";
 
 import { initApplicationDatabase } from "../../fixtures/db";
 import makeDb, { closeDb } from "../../fixtures/db/db";
 import { conditionsEnum, grantsEnum, defaultRoles } from "../../fixtures/db/authorization";
 
 import makeMailpyDb, { MailpyDB } from "./mailpy-db";
-import { makeCondition, makeGroup, Condition, Group } from "../entities";
+import { makeCondition, makeGroup, Condition, Group, ConditionName, makeEntry } from "../entities";
 import { DatabaseDuplicatedKeyError, InvalidPropertyError } from "../helpers/errors";
 
 beforeAll(async () => await initApplicationDatabase({ makeDb }));
@@ -53,8 +53,51 @@ describe("mailpy db", () => {
     expect(await mailpyDb.deleteGroup(foundGroup)).toBe(false);
   });
 
-  it("create entity", () => {});
+  it("create entity", async () => {
+    const condition = await mailpyDb.findConditionByName(ConditionName.SUPERIOR_THAN);
+    const group = await mailpyDb.createGroup(
+      makeGroup({
+        enabled: false,
+        name: faker.name.findName(),
+        desc: faker.lorem.paragraph(),
+      })
+    );
+    const _defaults = {
+      alarm_values: "50",
+      email_timeout: 1600,
+      emails: [""],
+      subject: "",
+      unit: "",
+      warning_message: "",
+      pvname: faker.name.title(),
+    };
+    const entry = makeEntry({
+      condition,
+      group,
+      ..._defaults,
+    });
 
+    const newEntry = await mailpyDb.createEntry(entry);
+    expect(newEntry.id).toBeDefined();
+    expect(newEntry.group).toStrictEqual(entry.group);
+    expect(newEntry.condition).toStrictEqual(entry.condition);
+
+    expect(newEntry.alarm_values).toBe(entry.alarm_values);
+    expect(newEntry.email_timeout).toBe(entry.email_timeout);
+    expect(newEntry.emails).toStrictEqual(entry.emails);
+    expect(newEntry.subject).toBe(entry.subject);
+    expect(newEntry.unit).toBe(entry.unit);
+    expect(newEntry.warning_message).toBe(entry.warning_message);
+    expect(newEntry.pvname).toBe(entry.pvname);
+
+    const foundEntry = await mailpyDb.findEntryById(newEntry.id);
+    expect(foundEntry).toStrictEqual(newEntry);
+
+    const newAlarmValues = "123.1234";
+    const newEntryParams = makeEntry({ ...newEntry, alarm_values: newAlarmValues });
+    const updateEntry = await mailpyDb.updateEntry(newEntryParams);
+    expect(updateEntry).toStrictEqual(newEntryParams);
+  });
   it("find all conditions", async () => {
     const conditions = await mailpyDb.findAllConditions();
     expect(conditions.length).toBe(Object.keys(conditionsEnum).length);
