b09b154063704ddd04557bc5977589c5d4848e2e
carneirofc <claudiofcarneiro@hotmail.com>
2021-07-02 13:57:45 -0300

    use-cases: Update interfaces

--- src/use-cases/index.ts
+++ src/use-cases/index.ts
@@ -4,12 +4,16 @@ import makeListConditions from "./list-conditions";
 import makeListGroups from "./list-groups";
 import makeListEntries from "./list-entries";
 import makeUserLogin from "./login-user";
+import makeAddGroup from "./add-group";
+import makeAddEntry from "./add-entry";
 
 import { getAzureUserInfo as getExternalUserInfo } from "../helpers/azure";
 
 export const listConditions = makeListConditions({ mailpyDb });
 export const listGroups = makeListGroups({ mailpyDb });
 export const listEntries = makeListEntries({ mailpyDb });
+export const addGroup = makeAddGroup({ mailpyDb });
+export const addEntry = makeAddEntry({ mailpyDb });
 
 export const userLogin = makeUserLogin({
   mailpyDb,
---
+++ src/use-cases/mailpy.test.ts
@@ -0,0 +1,47 @@
+import faker from "faker";
+
+import makeDb, { closeDb } from "../../fixtures/db/db";
+import { initApplicationDatabase } from "../../fixtures/db";
+
+import makeMailpyDb, { MailpyDB } from "../data-access/mailpy-db";
+import makeUsersDb, { UsersDb } from "../data-access/users-db";
+
+import makeAddGroup from "./add-group";
+import makeAddEntry from "./add-entry";
+import { ConditionName } from "../entities";
+
+beforeAll(async () => await initApplicationDatabase({ makeDb }));
+
+afterAll(async () => await closeDb());
+
+describe("perform a user login", () => {
+  let mailpyDb: MailpyDB;
+  let usersDb: UsersDb;
+
+  beforeEach(async () => {
+    mailpyDb = makeMailpyDb({ makeDb });
+    usersDb = makeUsersDb({ makeDb });
+  });
+
+  it("create group and entry", async () => {
+    const addGroup = makeAddGroup({ mailpyDb });
+    const param = { name: faker.name.findName(), desc: faker.lorem.sentence(), enabled: false };
+    const newGroup = await addGroup(param);
+    expect(param.name).toBe(newGroup.name);
+    expect(param.desc).toBe(newGroup.desc);
+    expect(param.enabled).toBe(newGroup.enabled);
+
+    const addEntry = makeAddEntry({ mailpyDb });
+    const resEntry = await addEntry({
+      group_id: newGroup.id,
+      alarm_values: "60",
+      condition_name: ConditionName.INFERIOR_THAN,
+      email_timeout: faker.datatype.number(),
+      emails: [faker.internet.email()],
+      pvname: faker.name.findName(),
+      subject: faker.name.lastName(),
+      unit: faker.datatype.string(),
+      warning_message: faker.datatype.string(),
+    });
+  });
+});
